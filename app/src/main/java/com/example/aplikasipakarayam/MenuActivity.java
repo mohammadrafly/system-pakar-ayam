1728ffbec872bea638a4064bc881b8153c4316d9
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-05-25 16:17:55 +0700

    update ui

--- app/src/main/java/com/example/aplikasipakarayam/MenuActivity.java
+++ app/src/main/java/com/example/aplikasipakarayam/MenuActivity.java
@@ -1,36 +1,87 @@
 package com.example.aplikasipakarayam;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-public class MenuActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MenuActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private HomeFragment homeFragment;
+    private DiagnosaFragment diagnosaFragment;
+    private BantuanFragment bantuanFragment;
+    private PenyakitFragment penyakitFragment;
+    private TentangFragment tentangFragment;
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        View backButton = findViewById(R.id.buttonBack);
-        backButton.setOnClickListener(v -> finish());
+        View backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> onBackPressed());
 
-        View bantuanButton = findViewById(R.id.bantuanButton);
-        bantuanButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MenuActivity.this, BantuanActivity.class);
-            startActivity(intent);
-        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        View tentangButton = findViewById(R.id.tentangButton);
-        tentangButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MenuActivity.this, TentangActivity.class);
-            startActivity(intent);
-        });
+        initializeFragments();
 
-        View diagnosaButton = findViewById(R.id.diagnosaButton);
-        diagnosaButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MenuActivity.this, DiagnosaActivity.class);
-            startActivity(intent);
-        });
+        fragmentManager = getSupportFragmentManager();
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    private void initializeFragments() {
+        homeFragment = new HomeFragment();
+        diagnosaFragment = new DiagnosaFragment();
+        bantuanFragment = new BantuanFragment();
+        penyakitFragment = new PenyakitFragment();
+        tentangFragment = new TentangFragment();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int selectedItemId = item.getItemId();
+
+        if (selectedItemId == R.id.home) {
+            replaceFragment(homeFragment);
+            return true;
+        } else if (selectedItemId == R.id.diagnosa) {
+            replaceFragment(diagnosaFragment);
+            return true;
+        } else if (selectedItemId == R.id.bantuan) {
+            replaceFragment(bantuanFragment);
+            return true;
+        } else if (selectedItemId == R.id.penyakit) {
+            replaceFragment(penyakitFragment);
+            return true;
+        } else if (selectedItemId == R.id.tentang) {
+            replaceFragment(tentangFragment);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        fragmentManager.beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .addToBackStack(null) // Add the transaction to the back stack
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            fragmentManager.popBackStack(); // Navigate back to the previous fragment
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/aplikasipakarayam/TentangFragment.java
@@ -0,0 +1,20 @@
+package com.example.aplikasipakarayam;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TentangFragment extends Fragment {
+    public TentangFragment(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tentang, container, false);
+    }
+}
