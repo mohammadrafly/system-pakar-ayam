86044480ab856128b421c69342123f6b7b82b1a0
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-07-09 05:11:31 +0700

    new model

--- app/src/main/java/com/example/aplikasipakarayam/PenyakitModel.java
+++ app/src/main/java/com/example/aplikasipakarayam/PenyakitModel.java
@@ -15,8 +15,8 @@ public class PenyakitModel {
 
     private static final double[] PROBABILITAS_PENYAKIT = {
             0.3,
-            0.4,
             0.3,
+            0.4,
             0.3,
             0.4,
             0.3
@@ -25,67 +25,58 @@ public class PenyakitModel {
     private static final Map<String, Map<String, Double>> DATA_PENYAKIT = new HashMap<>();
 
     static {
-        // Gumboro
         Map<String, Double> gumboroGejala = new HashMap<>();
         gumboroGejala.put("Ayam tampak lesu", 0.6);
         gumboroGejala.put("Sayap menggantung dan lemas", 0.6);
-        gumboroGejala.put("Napsu makan menurun", 0.4);
+        gumboroGejala.put("Napsu makan pada ayam menurun", 0.4);
         gumboroGejala.put("Tubuh ayam bergetar", 0.4);
         DATA_PENYAKIT.put("Gumboro", gumboroGejala);
 
         // Infectious Coryza
         Map<String, Double> coryzaGejala = new HashMap<>();
-        coryzaGejala.put("Terjadi pembengkakan di daerah mata dan muka", 0.7);
-        coryzaGejala.put("Produksi telur menurun", 0.7);
-        coryzaGejala.put("Ngorok basah", 0.6);
+        coryzaGejala.put("Napsu makan pada ayam menurun", 0.8);
+        coryzaGejala.put("Ngorok basah", 0.8);
+        coryzaGejala.put("Produksi telur menurun", 0.6);
         coryzaGejala.put("Keluar lendir pada hidung", 0.6);
-        coryzaGejala.put("Napsu makan menurun", 0.4);
+        coryzaGejala.put("Terjadi pembengkakan di daerah mata dan muka", 0.4);
         DATA_PENYAKIT.put("Infectious Coryza", coryzaGejala);
 
         // Avian Influenza
         Map<String, Double> influenzaGejala = new HashMap<>();
-        influenzaGejala.put("Ayam bersin", 0.8);
-        influenzaGejala.put("Mata berair", 0.8);
-        influenzaGejala.put("Sinusitis", 0.7);
+        influenzaGejala.put("Ayam bersin", 0.6);
+        influenzaGejala.put("Sinusitis", 0.6);
+        influenzaGejala.put("Mata berair", 0.4);
         influenzaGejala.put("Ayam batuk", 0.4);
-        influenzaGejala.put("Badan lemah", 0.4);
-        influenzaGejala.put("Diare", 0.4);
         DATA_PENYAKIT.put("Avian Influenza", influenzaGejala);
 
         // Infectious Bronchitis
         Map<String, Double> bronchitisGejala = new HashMap<>();
-        bronchitisGejala.put("Napas mengih-mengih", 0.8);
-        bronchitisGejala.put("Leleran di hidung", 0.8);
-        bronchitisGejala.put("Mata berair", 0.8);
-        bronchitisGejala.put("Ayam bersin", 0.8);
-        bronchitisGejala.put("Pembengkakan di sinus", 0.7);
-        bronchitisGejala.put("Ngorok basah", 0.6);
-        bronchitisGejala.put("Ayam batuk", 0.4);
-        bronchitisGejala.put("Lesu", 0.4);
-        bronchitisGejala.put("Napsu makan & Berat badan turun drastis", 0.4);
+        bronchitisGejala.put("Ayam mengap-mengap", 0.8);
+        bronchitisGejala.put("Ayam batuk", 0.6);
+        bronchitisGejala.put("Ayam bersin", 0.6);
+        bronchitisGejala.put("Pembengkakan di sinus", 0.6);
         DATA_PENYAKIT.put("Infectious Bronchitis", bronchitisGejala);
 
         // New Castle Disease
         Map<String, Double> newCastleGejala = new HashMap<>();
-        newCastleGejala.put("Bulu tampak jatuh kebawah", 0.8);
+        newCastleGejala.put("Sayap menggantung dan lemas", 0.6);
+        newCastleGejala.put("Diare kehijauan", 0.4);
         newCastleGejala.put("Ayam mengap-mengap", 0.8);
-        newCastleGejala.put("Ayam bersin", 0.8);
-        newCastleGejala.put("Ayam batuk", 0.8);
-        newCastleGejala.put("Badan ayam bergetar", 0.6);
+        newCastleGejala.put("Ayam bersin", 0.4);
+        newCastleGejala.put("Napsu makan pada ayam menurun", 0.6);
+        newCastleGejala.put("Tubuh ayam bergetar", 0.6);
+        newCastleGejala.put("Ayam tampak lesu", 0.6);
+        newCastleGejala.put("Jengger berwarna keabuan", 0.8);
         newCastleGejala.put("Ngorok basah", 0.6);
-        newCastleGejala.put("Ayam tampak lesu", 0.4);
-        newCastleGejala.put("Jengger berwarna keabuan", 0.4);
-        newCastleGejala.put("Napsu makan menurun", 0.4);
-        newCastleGejala.put("Diare Kehijauan", 0.4);
         DATA_PENYAKIT.put("New Castle Disease", newCastleGejala);
 
         // Pullorum Disease
         Map<String, Double> pullorumGejala = new HashMap<>();
-        pullorumGejala.put("Sayap menggantung dan lemas", 0.7);
+        pullorumGejala.put("Napsu makan pada ayam menurun", 0.6);
+        pullorumGejala.put("Sayap menggantung dan lemas", 0.8);
         pullorumGejala.put("Kotoran berwarna putih", 0.6);
-        pullorumGejala.put("Lumpuh karena arthritis", 0.5);
-        pullorumGejala.put("Napsu makan pada ayam menurun", 0.4);
-        pullorumGejala.put("Jengger berwarna keabuan", 0.4);
+        pullorumGejala.put("Jengger berwarna keabuan", 0.8);
+        pullorumGejala.put("Lumpuh karena arthritis", 0.4);
         pullorumGejala.put("Ayam tampak lesu", 0.4);
         DATA_PENYAKIT.put("Pullorum Disease", pullorumGejala);
     }
@@ -94,33 +85,38 @@ public class PenyakitModel {
         StringBuilder hasilDeteksi = new StringBuilder();
 
         double[] probabilitasPenyakit = new double[PENYAKIT.length];
-        double totalProbabilitasGejala = 0.0;
+        double totalAkumulasiGejala = 0.0;
 
         for (int i = 0; i < PENYAKIT.length; i++) {
             String penyakit = PENYAKIT[i];
             double probabilitasPenyakitAwal = PROBABILITAS_PENYAKIT[i];
             Map<String, Double> gejalaPenyakit = DATA_PENYAKIT.get(penyakit);
 
-            double probabilitasGejala = 1.0;
+            double akumulasiGejala = 0.0;
             for (String gejala : gejalaTerpilih) {
                 if (gejalaPenyakit.containsKey(gejala)) {
-                    double probabilitas = gejalaPenyakit.get(gejala);
-                    probabilitasGejala *= probabilitas;
-                } else {
-                    probabilitasGejala *= 0.1; // Faktor ketidaktahuan
+                    double probabilitasGejala = gejalaPenyakit.get(gejala);
+                    akumulasiGejala += probabilitasGejala;
                 }
             }
 
-            probabilitasPenyakit[i] = probabilitasPenyakitAwal * probabilitasGejala;
-            totalProbabilitasGejala += probabilitasPenyakit[i];
+            probabilitasPenyakit[i] = akumulasiGejala * probabilitasPenyakitAwal;
+            totalAkumulasiGejala += probabilitasPenyakit[i];
+
+            // Logcat perhitungan
+            System.out.println("Penyakit: " + penyakit);
+            System.out.println("Probabilitas Awal: " + probabilitasPenyakitAwal);
+            System.out.println("Akumulasi Gejala: " + akumulasiGejala);
+            System.out.println("Probabilitas Penyakit: " + probabilitasPenyakit[i]);
+            System.out.println("-----------------------------------------");
         }
 
-        if (totalProbabilitasGejala != 0) {
+        if (totalAkumulasiGejala != 0) {
             for (int i = 0; i < PENYAKIT.length; i++) {
                 String penyakit = PENYAKIT[i];
-                double probabilitas = probabilitasPenyakit[i] / totalProbabilitasGejala;
+                double probabilitas = (probabilitasPenyakit[i] / totalAkumulasiGejala) * 100;
 
-                hasilDeteksi.append(penyakit).append(": ").append(probabilitas * 100).append("%\n");
+                hasilDeteksi.append(penyakit).append(": ").append(probabilitas).append("%\n");
             }
         }
 
